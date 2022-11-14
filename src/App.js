ffdd70bd81a5b10c9597bf7ca89d82d4ce8ded83
learnReactgithub <sonukushwah362@gmail.com>
2022-11-14 15:50:47 +0530

    useEffect hooks life cycle method

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  useEffect(()=>{
+    console.log("Mount Call");
+  })
+  const [count,setcount]=useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h4>UseEffect Hooks</h4>
+    <p>{count}</p>
+    <button onClick={()=>setcount(count+1)}>Update State</button>
+    </>
   );
 }
 
